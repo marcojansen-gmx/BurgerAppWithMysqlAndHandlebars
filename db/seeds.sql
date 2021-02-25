5f4e7973344edf8bafd6b84ba24f2cbe6fb01856
Marco Jansen <marco.jansen@gmx.us>
2021-02-25 16:36:59 +0800

    devour button position switched in index.handlebars

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,24 @@
-INSERT INTO burgers (name) VALUES ('Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Hamburger');
-INSERT INTO burgers (name, devoured) VALUES ('Whopper', true);
-INSERT INTO burgers (name, devoured) VALUES ('BigMac', true);
-INSERT INTO burgers (name, devoured) VALUES ('Chicken Burger', true);
-INSERT INTO burgers (name) VALUES ('McRib');
+INSERT INTO
+  burgers (name)
+VALUES
+  ('Cheeseburger');
+INSERT INTO
+  burgers (name)
+VALUES
+  ('Hamburger');
+INSERT INTO
+  burgers (name, devoured)
+VALUES
+  ('Whopper', true);
+INSERT INTO
+  burgers (name, devoured)
+VALUES
+  ('BigMac', true);
+INSERT INTO
+  burgers (name, devoured)
+VALUES
+  ('Chicken Burger', true);
+INSERT INTO
+  burgers (name)
+VALUES
+  ('McRib');
