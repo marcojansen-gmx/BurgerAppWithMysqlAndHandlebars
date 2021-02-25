6a8c2f24d31d5b0aaa837a5adeee5fb70732b39f
Marco Jansen <marco.jansen@gmx.us>
2021-02-25 16:32:14 +0800

    css layout and bootstrap grid added

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
 const burger = require('../models/burger.js');
 
 router.get('/', (req, res) => {
